046af57eb688716629c9b1a4e1e19706c21a7ec3
Avishai Melamed <avishay2008@gmail.com>
2022-04-29 14:49:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mirror_start/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.mirror_start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button send;
+    EditText e1;
+    TcpClient mTcpClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1 = (EditText) findViewById(R.id.editText);
+        send =(Button) findViewById(R.id.send_button);
+    }
+
+    public void send_message(View view){
+        //Connet to server
+        new ConnectTask().execute("");
+
+        //call sendMessage
+        if (mTcpClient != null){
+            mTcpClient.sendMessage(e1.getText().toString());
+        }
+
+        if (mTcpClient != null) {
+            mTcpClient.stopClient();
+        }
+    }
+
+
+    public class ConnectTask extends AsyncTask<String, String, TcpClient> {
+
+        @Override
+        protected TcpClient doInBackground(String... message) {
+
+            //we create a TCPClient object
+            mTcpClient = new TcpClient(new TcpClient.OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+            });
+            mTcpClient.run();
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            //response received from server
+            Log.d("test", "response " + values[0]);
+            //process server response here....
+
+        }
+    }
+
+}
