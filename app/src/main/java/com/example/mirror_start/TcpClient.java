e38792e69eb2ec218a397bbb28c0fbdbd45dee2b
Avishai Melamed <avishay2008@gmail.com>
2022-04-29 18:29:22 +0300

    first commit, first client creation, need to fix

--- app/src/main/java/com/example/mirror_start/TcpClient.java
+++ app/src/main/java/com/example/mirror_start/TcpClient.java
@@ -12,7 +12,7 @@ import java.net.Socket;
 public class TcpClient {
 
     public static final String TAG = TcpClient.class.getSimpleName();
-    public static final String SERVER_IP = "192.168.1.8"; //server IP address
+    public static final String SERVER_IP = "192.168.1.116"; //server IP address
     public static final int SERVER_PORT = 1234;
     // message to send to the server
     private String mServerMessage;
